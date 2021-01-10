a438f3747828d4a0eb021eb7ca5797ba59383c22
Srijan Das <srijandas18.sd@gmail.com>
2021-01-10 15:06:52 +0530

    initial commit

---
+++ tweetBot.py
@@ -0,0 +1,44 @@
+import tweepy
+from SecretKeys import *
+import time
+
+FILE_NAME = "last_seen.txt"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+
+def retrieve_last_seen_id(file_name):
+    f_read = open(file_name, 'r')
+    last_seen_id = int(f_read.read().strip())
+    f_read.close()
+    return last_seen_id
+
+
+def store_last_seen_id(last_seen_id, file_name):
+    f_write = open(file_name, 'w')
+    f_write.write(str(last_seen_id))
+    f_write.close()
+    return
+
+
+def reply_to_tweets():
+    print('retrieving and replying to tweets...')
+    last_seen_id = retrieve_last_seen_id(FILE_NAME)
+   
+    mentions = api.mentions_timeline(last_seen_id, tweet_mode='extended')
+    for mention in reversed(mentions):
+        print(str(mention.id) + ' - ' + mention.full_text)
+        last_seen_id = mention.id
+        store_last_seen_id(last_seen_id, FILE_NAME)
+        if '#helloworld' in mention.full_text.lower():
+            print('found #helloworld!')
+            print('responding back...')
+            api.update_status(f"@{mention.user.screen_name} #HelloWorld back to you!", mention.id)
+
+
+while True:
+    reply_to_tweets()
+    time.sleep(10)
